df8181f3cf9698fc78524372309a778fba271655
p31d4 <166958893+p31d4@users.noreply.github.com>
2022-03-05 21:58:57 +0000

    pcap file added

--- icmp/ref_cmd.sh
+++ icmp/ref_cmd.sh
@@ -10,3 +10,6 @@ nping -c 1 --icmp --icmp-type 8 127.0.0.1
 
 #nping -c 1 --icmp --icmp-type 8 -d3 127.0.0.1
 #nping -c 1 --icmp --icmp-type 8 --debug 127.0.0.1
+
+# tcpdump cmd used to capture pcap file
+#tcpdump -i lo "icmp[0]==8 || icmp[0]==0" -w capture.pcap
